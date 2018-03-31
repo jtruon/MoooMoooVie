d1fd26e6f1ddb70982338d8788e641fddc4be9fa
jtruon <34013869+jtruon@users.noreply.github.com>
2018-03-31 12:39:44 -0500

    Add files via upload
    added column "character" for entity movies_stars. and update insertMovie_stars.

--- InsertMovie_Stars.sql
+++ InsertMovie_Stars.sql
@@ -2,14 +2,14 @@
 -- Query: SELECT * FROM movies2017.movies_stars
 LIMIT 0, 1000
 
--- Date: 2018-03-31 00:23
+-- Date: 2018-03-31 12:37
 */
-INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`) VALUES (1,1);
-INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`) VALUES (1,2);
-INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`) VALUES (1,3);
-INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`) VALUES (2,4);
-INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`) VALUES (2,5);
-INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`) VALUES (2,6);
-INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`) VALUES (3,7);
-INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`) VALUES (3,8);
-INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`) VALUES (3,9);
+INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`,`character`) VALUES (1,1,'Bill Denbrough');
+INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`,`character`) VALUES (1,2,'Pennywise');
+INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`,`character`) VALUES (1,3,'Richie Tozier');
+INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`,`character`) VALUES (2,4,'Chris Washington');
+INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`,`character`) VALUES (2,5,'Rose Armitage');
+INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`,`character`) VALUES (2,6,'Dean Armitage');
+INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`,`character`) VALUES (3,7,'Diana');
+INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`,`character`) VALUES (3,8,'Steve Trevor');
+INSERT INTO `movies_stars` (`Movies_idMovies`,`Stars_idStars`,`character`) VALUES (3,9,'Antiope');
